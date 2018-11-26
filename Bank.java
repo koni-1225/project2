36babe7547e86ad15dc13a7c8a5397bb11d7d501
koni-1225 <45259107+koni-1225@users.noreply.github.com>
2018-11-26 14:56:57 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,63 +1,64 @@
-import java.lang.*;
+
+
+
+
 import java.util.*;
+import java.lang.*;
 public class Bank
 {
-	public int bal=0;
-	public void Deposit(int d)
-	{
-		//System.out.println("Enter amount to be deposited");
-		bal=bal+d;
-		System.out.println(bal);
-	}
-	public void Withdraw(int n)
-	{
-		System.out.println("Enter amount to withdrawl");
-		if(bal<=n)
-		{
-			bal=bal-n;
-			System.out.println("take money");
-		}
-		else
-		{
-			System.out.println("insufficient funds");
-		}
-	}
-	public void check()
-	{
-		System.out.println(bal);
-	}
-	
-	
-		
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		Bank b=new Bank();
-		String accnumber= "3000123456";
-		String name=sc.nextLine();
-		int ch;
-		System.out.println("Enter ur choice");
-		System.out.printf("1.Deposit"+"2.Withdraw"+"3.check");
-		while(true)
-		{
-		//switch(ch)
-		//{
-			/*case 1:System.out.println("Enter amount to be deposited");
-			       int d=sc.nextInt();
-			       b.Deposit(d);
-			       break;
-			case 2:System.out.println("Enter amount to withdrawl");
-			       int n=sc.nextInt();
-			       b.Withdraw(n);
-			       break;
-			case 3:b.check();
-			       break;
-			
-		}/*
-	}
-	
-	}
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,ch,amt,bal=600,min=500;
+        String s="3000123456";
+        System.out.print("Enter username:");
+        String un=sc.next();
+        String name=sc.next();
+        String s4=name.substring(0,3)+un.substring(6,10);
+        System.out.println("Enter password: ");
+        String p=sc.next();
+        if(un.equals(s) && p.equals(s4));
+        {
+            while(true)
+            {
+                System.out.println("1.Deposit  2.Withdraw   3.Transfer  4.Check Balance 5.Exit");
+                ch=sc.nextInt();
+                switch(ch)
+                {
+                    case 1:
+                        System.out.println("Enter amount to deposit:");
+                        amt=sc.nextInt();
+                        bal=bal+amt;
+                        break;
+                    case 2:
+                        System.out.println("Enter how much to amount to withdraw");
+                        amt=sc.nextInt();
+                        if(bal<min || bal<0)
+                        {
+                            System.out.println("No minimum balance");
+                        }
+                       
+                        else
+                            bal=bal-amt;
+                        break;
+                    case 3:
+                        System.out.println("Enter how much to amount to transfer");
+                        amt=sc.nextInt();
+                        if(bal<min || bal<0)
+                        {
+                            System.out.println("No minimum balance");
+                        }
+                        else
+                            bal=bal-amt;
+                        break;
+                    case 4:
+                        System.out.println("Available balance is "+bal);
+                        break;
+                    case 5:
+                        System.exit(0);
+                }
+            }   
+        }
+    }
 }
-			       
-	
- 
